d0bb7d143f35bae220533aeb283700f972fb90c2
nizar.grami <nizar.grami@gmail.com>
2022-12-21 09:32:39 +0100

    adding the function letterI

--- letters.py
+++ letters.py
@@ -61,4 +61,21 @@ def letterN ():
 
 
 
+def letterI ():
+    ### the purpose of this function is to draw the seond letter, I, which contains three parts:
+
+       # drawing the letter I, first part
+    
+    goto (-130,0) # moving the turtle to the near field next the letter N
+    setheading(0)  # change the direction of the turtle's head, to make the head towards north
+    begin_fill()
+    pendown()
+    forward(300)
+    circle(10,180)
+    forward(300)
+    circle(10,180)
+    end_fill()
+    penup()
+
+
 
