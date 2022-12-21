73ce63e8c5ba60b3ccf6ad939c0a63f80a2ae3cc
nizar.grami <nizar.grami@gmail.com>
2022-12-21 09:29:35 +0100

    describing the module

---
+++ letters.py
@@ -0,0 +1,8 @@
+## This file is the module which contains all the necessay functions to draw the letters
+# every letter will be drawn by one function
+# it will be called by the main file.
+
+
+
+# importing the module turtle and all of its methods
+from turtle import *
