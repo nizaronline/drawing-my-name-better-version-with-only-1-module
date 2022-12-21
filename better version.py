47e808bf5c93e9b5d5cbf394851fd56773745e3e
nizar.grami <nizar.grami@gmail.com>
2022-12-21 09:42:23 +0100

    peaufiner les détails ;-)

--- better version.py
+++ better version.py
@@ -1,15 +1,11 @@
-# This program is intended to draw my name using the language Python and the module turtle
-# this version is used with one module which contains functions
-# every function consists in drawing only one letter.
-
-
+# This program is intended to draw my name using the language Python and the module turtle,
+# this version is used with one module which contains 5 functions.
+# Every function consists in drawing only one letter.
 
 # importing the module turtle and all of its methods
 from turtle import *
 
-
 # importing the module ' letters ', so that the functions can be called.
-
 from letters import letterN, letterI, letterZ, letterA, letterR
 
 #defining the screen as an object
@@ -20,9 +16,9 @@ screen.title("Writing My Name, another edition")
 # defining the turtle as the object which makes the drawing
 color("black")
 speed(0)
-mode("logo") # to make the turtle head to the north, and the postitve angles for counterclockwise rotation
+mode("logo") # to make the turtle head to the north
 penup()
-hideturtle() # it is optinal, just to hide the object to make better animation
+hideturtle() # it is optinal, just to hide the object and to make better animation
 
 def main():
     letterN()
@@ -32,9 +28,9 @@ def main():
     letterR()
 
 
-
-
 main()
+
+### This program is written by NIZAR.
     
     
 
