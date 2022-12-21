877344c59af1bf9cbb8d743f58655fa448320af9
nizar.grami <nizar.grami@gmail.com>
2022-12-21 09:27:14 +0100

    describing the program, making the objects : screen and turtle

---
+++ better version.py
@@ -0,0 +1,27 @@
+# This program is intended to draw my name using the language Python and the module turtle
+# this version is used with one module which contains functions
+# every function consists in drawing only one letter.
+
+
+
+# importing the module turtle and all of its methods
+from turtle import *
+
+#defining the screen as an object
+screen=Screen()
+screen.bgcolor("white")
+screen.title("Writing My Name, another edition")
+
+# defining the turtle as the object which makes the drawing
+color("black")
+speed(0)
+mode("logo") # to make the turtle head to the north, and the postitve angles for counterclockwise rotation
+penup()
+hideturtle() # it is optinal, just to hide the object to make better animation
+
+
+
+
+
+
+
